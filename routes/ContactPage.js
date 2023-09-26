8f39ee883301124b2e3e02a73a95439a707f96c6
Tsz Him Ng, Lucas <l34229776@gmail.com>
2023-09-26 16:03:14 -0400

    Re-add jquery script

--- routes/ContactPage.js
+++ routes/ContactPage.js
@@ -5,9 +5,9 @@ router.get('/', function(req, res, next) {
     res.render('pages/ContactPage/index', {title : "Contact Me"});
   });
   
-  router.post('/', function(req, res) {
-    const visitorName = req.body.name;
-    res.redirect('/');
-  });
+  // router.post('/', function(req, res) {
+  //   const visitorName = req.body.name;
+  //   res.redirect('/');
+  // });
 module.exports = router;
   
--- routes/MainPage.js
+++ routes/MainPage.js
@@ -3,7 +3,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  const visitorName = req.body.name;
+  var visitorName = req.body.name;
+  // const visitorName = "get";
+  res.render('pages/MainPage/index', {title : "My Portfolio", visitor: visitorName });
+});
+
+router.post('/', function(req, res) {
+  var visitorName = req.body.name;
+  // const visitorName = "post";
   res.render('pages/MainPage/index', {title : "My Portfolio", visitor: visitorName });
 });
 
